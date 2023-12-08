d7012fc6b8ac0024493ef522f1c4c829ee5fc4b9
Hafiz Muhammad waqas <waqasowais548@gmail.com>
2023-12-08 22:13:15 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/////////////////////////////////////////////Javascript Task 3//////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////javascript Task 3//////////////////////////////////////////////////////////////////////
 
 ////////////////////Find even and odd numbers in an array////////////////////////
 // var numberArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
@@ -17,15 +17,15 @@
 
 ////////////////////Push even numbers in even array and odd numbers in odd array////////////////////////
 
-var evennumbers = [];
+var evennumberss = [];
 var oddnumbers = [];
 for(i=1; i<=20;i++){
     if(i%2===0){
-        evennumbers.push(i);
+        evennumberss.push(i);
     }
     else{
         oddnumbers.push(i);
     }
 }
-document.writeln("<h1> Even numbers: "+ evennumbers + "</h1><br>");
+document.writeln("<h1> Even numbers: "+ evennumberss + "</h1><br>");
 document.writeln("<h1> Odd numbers: "+ oddnumbers + "</h1>");
